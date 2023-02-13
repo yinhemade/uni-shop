716e1e3fd08b14db0c85656bc672af03a89bb95f
LuLuLin <764613693@qq.com>
2023-02-13 21:28:16 +0800

    完成搜索功能开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,8 @@
 <template>
 	<view>
+		<!-- 使用自定义的搜索组件 -->
+		<my-search @click="gotoSearch"></my-search>
+
 		<view class="scroll-view-container">
 		    <!-- 左侧的滚动视图区域 -->
 		    <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
@@ -60,7 +63,8 @@
 			// 获取当前系统的信息
 			const sysInfo = uni.getSystemInfoSync()
 			// 为 wh 窗口可用高度动态赋值
-			this.wh = sysInfo.windowHeight
+			// 可用高度 = 屏幕高度 - navigationBar高度 - tabBar高度 - 自定义的search组件高度
+			this.wh = sysInfo.windowHeight - 50
 			// 调用获取分类列表数据的方法
 			this.getCateList()
 		},
@@ -89,7 +93,13 @@
 			  uni.navigateTo({
 			    url: `/subpkg/goods_list/goods_list?cid=${item3.cat_id}`
 			  })
-			}
+			},
+			// 跳转到分包中的搜索页面
+		    gotoSearch() {
+				uni.navigateTo({
+				    url: '/subpkg/search/search'
+				})
+		    }
 		}
 	}
 </script>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,10 @@
 <template>
 	<view>
+		<!-- 使用自定义的搜索组件 -->
+		<view class="search-box">
+		  <my-search @click="gotoSearch"></my-search>
+		</view>
+
 		<!-- 轮播图 -->
 		<swiper indicator-dots autoplay :interval="3000" :duration="1000" circular>
 			<swiper-item v-for="item in swiperList" :key="item.goods_id" >
@@ -91,7 +96,14 @@
 						url: '/pages/cate/cate'
 					})
 				}
+			},
+			// 自定义事件=>顶部搜索栏
+			gotoSearch() {
+			  uni.navigateTo({
+			    url: '/subpkg/search/search'
+			  })
 			}
+
 		}
 		
 	}
@@ -132,4 +144,13 @@ swiper {
 		justify-content: space-around;
 	}
 }
+.search-box {
+  // 设置定位效果为“吸顶”
+  position: sticky;
+  // 吸顶的“位置”
+  top: 0;
+  // 提高层级，防止被轮播图覆盖
+  z-index: 999;
+}
+
 </style>
