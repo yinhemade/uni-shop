207faf0659443b7d612cda4f2021722019217dfd
LuLuLin <764613693@qq.com>
2023-02-17 00:53:07 +0800

    完成登录与用户界面，不完成支付

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # uni-shop
 小程序商城-学习
 **仅用作个人练手**
+10.2 登录功能
+黑马的接口无法使用，获取不了token值
+注：新版--2023/2/16时，getUserProfile已无法获取头像和昵称 
+处理：
+1.用匿名方式登录，随手编个token，我这是直接用wx.login（）的code值直接当作是token使用
+2.用button的open-type="chooseAvatar"，调用 @chooseavatar="事件"，方法会直接获得一个参数，可作为头像的src
+3.获得昵称，可以在input里设置type="nickname" 然后用@input=“事件”一直获取输入，但是在模拟器短有问题，真机没问题
--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -1,3 +1,4 @@
+<!-- 结算组件 -->
 <template>
 	<!-- 最外层的容器 -->
 	<view class="my-settle-container">
@@ -12,22 +13,31 @@
 		</view>
 
 		<!-- 结算按钮 -->
-		<view class="btn-settle">结算({{checkedCount}})</view>
+		<view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
 	</view>
 </template>
 
 <script>
 	import {
 		mapGetters,
-		mapMutations
+		mapMutations,
+		mapState
 	} from 'vuex';
 	export default {
 		name: "my-settle",
 		data() {
-			return {};
+			return {
+				// 倒计时的秒数
+				seconds: 3,
+				// 定时器的 Id
+				timer: null
+			};
 		},
 		computed: {
 			...mapGetters('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount']),
+			...mapGetters('m_user', ['addstr']),
+			// token 是用户登录成功之后的 token 字符串
+			...mapState('m_user', ['token']),
 			// 是否全选
 			isFullCheck() {
 				return this.total === this.checkedCount
@@ -36,13 +46,75 @@
 		methods: {
 			//  m_cart 的 updateAllGoodsState 方法映射到组件中使用
 			...mapMutations('m_cart', ['updateAllGoodsState']),
+			...mapMutations('m_user', ['updateRedirectInfo']),
 			// label 的点击事件处理函数
 			changeAllState() {
 				console.log(!this.isFullCheck)
 				// 修改购物车中所有商品的选中状态
 				// !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
 				this.updateAllGoodsState(!this.isFullCheck)
+			},
+			// 点击了结算按钮
+			settlement() {
+				// 1. 先判断是否勾选了要结算的商品
+				if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+				// 2. 再判断用户是否选择了收货地址
+				if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+				// 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+				if (!this.token) return this.delayNavigate()
+			},
+			// 延迟导航到 my 页面
+			delayNavigate() {
+				this.seconds = 3
+				// 1. 展示提示消息，此时 seconds 的值等于 3
+				this.showTips(this.seconds)
+
+				// 1.创建定时器，每隔 1 秒执行一次   2. 将定时器的 Id 存储到 timer 中
+				this.timer = setInterval(() => {
+					// 让秒数自减 1
+					this.seconds--;
+
+					if (this.seconds <= 0) {
+						//  清除定时器
+						clearInterval(this.timer)
+
+						// 跳转到 my 页面
+						uni.switchTab({
+							url: '/pages/my/my',
+							// 页面跳转成功之后的回调函数
+							success: () => {
+								// 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+								this.updateRedirectInfo({
+									// 跳转的方式
+									openType: 'switchTab',
+									// 从哪个页面跳转过去的
+									from: '/pages/cart/cart'
+								})
+							}
+						})
+						return
+					}
+					// 根据最新的秒数，进行消息提示
+					this.showTips(this.seconds)
+				}, 1000)
+			},
+			// 展示倒计时的提示消息
+			showTips(n) {
+				// 调用 uni.showToast() 方法，展示提示消息
+				uni.showToast({
+					// 不展示任何图标
+					icon: 'none',
+					// 提示的消息
+					title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+					// 为页面添加透明遮罩，防止点击穿透
+					mask: true,
+					// 1.5 秒后自动消失
+					duration: 1500
+				})
 			}
+			// methods结尾
 		},
 	}
 </script>
